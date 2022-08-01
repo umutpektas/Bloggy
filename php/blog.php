378e6b5420d8b8cc123bf1bd268751a30e240014
Umut Pektaş <67418880+umutpektas@users.noreply.github.com>
2022-08-01 23:39:31 +0300

    Add files via upload

---
+++ php/blog.php
@@ -0,0 +1,15 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "php_project");
+
+    if(isset($_POST['submit'])){
+        $title = $_POST['title'];
+        $body = $_POST['body'];
+        $query = "INSERT INTO users (title,body) VALUES ('$title','$body')";
+        $result = mysqli_query($conn, $query);
+        if($result){
+            echo "<script>alert('Kayıt başarılı!')</script>";
+        }else{
+            echo "<script>alert('Kayıt başarısız!')</script>";
+        }
+    }
