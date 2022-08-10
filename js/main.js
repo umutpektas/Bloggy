64d359500b7798fe10ed1bcd847c9f4cedfbd7ec
Umut Pektaş <pektas.umut1@gmail.com>
2022-08-10 21:13:12 +0300

    footer fix

--- js/main.js
+++ js/main.js
@@ -4,24 +4,28 @@ const div = document.getElementById('konteyner');
 const div2 = document.getElementById('blog-div');
 const sign_in_div = document.getElementById('sign-in-div');
 const sign_up_div = document.getElementById('sign-up-div');
+const footer_div = document.getElementById('footer');
 
 sign_in_btn.addEventListener('click', function handleClick(){
     if(div.style.display === 'block'){
-         div.style.display = 'none';
+        div.style.display = 'none';
         sign_up_div.style.display = 'none';
         sign_in_div.style.display = 'block'; 
+        footer_div.style.position = 'fixed';
     }
 
     else if(div.style.display === 'none' && sign_up_div.style.display==='block'){
         div.style.display = 'none';
         sign_up_div.style.display = 'none';
         sign_in_div.style.display = 'block';
+        footer_div.style.position = 'fixed';
     }
 
     else if (div.style.display === 'none'){
         div.style.display = 'block';
         sign_up_div.style.display = 'none';
         sign_in_div.style.display = 'none';
+        footer_div.style.position = 'relative';
     }
 });
 
@@ -30,18 +34,21 @@ sign_up_btn.addEventListener('click', function handleClick(){
         div.style.display = 'none';
         sign_in_div.style.display = 'none';
         sign_up_div.style.display = 'block';
+        footer_div.style.position = 'fixed';
     }
 
     else if(div.style.display === 'none' && sign_in_div.style.display==='block'){
         div.style.display = 'none';
         sign_in_div.style.display = 'none';
         sign_up_div.style.display = 'block';
+        footer_div.style.position = 'fixed';
     }
 
     else if (div.style.display === 'none'){
         div.style.display = 'block';
         sign_in_div.style.display = 'none';
         sign_up_div.style.display = 'none';
+        footer_div.style.position = 'relative';
     }
 });
 
@@ -53,18 +60,21 @@ sign_in_btn.addEventListener('click', function handleClick(){
         div2.style.display = 'none';
         sign_up_div.display = 'none';
         sign_in_div.style.display = 'block';
+        footer_div.style.position = 'fixed';
     }
 
     else if(div2.style.display === 'none' && sign_up_div.style.display==='block'){
         div2.style.display = 'none';
         sign_up_div.style.display = 'none';
         sign_in_div.style.display = 'block';
+        footer_div.style.position = 'fixed';
     }
 
     else if (div2.style.display === 'none'){
         div2.style.display = 'flex';
         sign_up_div.style.display = 'none';
         sign_in_div.style.display = 'none';
+        footer_div.style.position = 'relative';
     }
 });
 
@@ -85,12 +95,14 @@ sign_up_btn.addEventListener('click', function handleClick(){
         div2.style.display = 'none';
         sign_in_div.style.display = 'none';
         sign_up_div.style.display = 'block';
+        footer_div.style.position = 'fixed';
     }
 
     else if(div2.style.display === 'none' && sign_in_div.style.display==='block'){
         div2.style.display = 'none';
         sign_in_div.style.display = 'none';
         sign_up_div.style.display = 'block';
+        footer_div.style.position = 'fixed';
     }
     
 
@@ -98,6 +110,7 @@ sign_up_btn.addEventListener('click', function handleClick(){
         div2.style.display = 'flex';
         sign_in_div.style.display = 'none';
         sign_up_div.style.display = 'none';
+        footer_div.style.position = 'relative';
     }
 });
 
